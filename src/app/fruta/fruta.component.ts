1ca44e3c78017731a8b67999bdcd5c79c64bf233
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-09-30 23:40:49 -0500

    Este es un commit de prueba
    La unica alteracion de este commit es en el componente fruta.component.ts, en el cual el unico cambio es de un salto de linea, este commit es solo para hacer una prueba con el editor de texto vim.
    
    Pierre David Maldonado Diaz
    pidmad92@gmail.com

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'fruta',
 	templateUrl: './fruta.component.html'
-	/*	Template en Linea 
+	/*	Template en Linea
 	template: `<h2>{{nombre_componente}}</h2><p>{{listado_frutas}}</p>`
 	*/
 })
 
 export class FrutaComponent{
-	
+
 	public nombre_componente = 'Componente de fruta';
 	public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
 }

9ca3c47202099ea28261685a89b169948e3469b7
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-10-01 09:13:59 -0500

    Altera principalmente fruta.component.ts
    Agrega una prueba de los tipos de datos, string, number, array, any, boolean; con los que soporta TypeScript, tambien se han creado un constructor, donde claramente se observa que este es lo primero que se carga en en la clase y donde se recomienda que se asignen los valores de las variables, y tambien se creo una funcion, que puede cambiar el valor de una variable, y tambien generaba una alerta, que podia ser cargada al cargar la funcion desde el constructor.
    
    Pierre David Maldonado Diaz
    pidmad@gmail.com

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -12,4 +12,25 @@ export class FrutaComponent{
 
 	public nombre_componente = 'Componente de fruta';
 	public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
+
+	public stringEjemplo: string;
+	public numeroEjemplo: number;
+	public arrayStringEjemplo: Array<String>;
+	public arrayAnyEjemplo: Array<any>;
+	public booleanEjemplo: boolean;
+
+	constructor(){
+		this.stringEjemplo = 'Ejemplo de String';
+		this.numeroEjemplo = 25;
+		this.arrayStringEjemplo = ['String1', 'String2'];
+		this.arrayAnyEjemplo = ['String1', 2];
+		this.booleanEjemplo = true;
+
+		this.funcionEjemplo();
+	}
+
+	funcionEjemplo(){
+		this.nombre_componente = 'Refruta xD';
+		alert('Hola Mundo ' + this.nombre_componente);
+	}
 }

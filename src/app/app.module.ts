8c96b7ccc43ce2e448d58b949c6ba36e84cf0d39
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-10-05 05:01:59 -0500

    Routing - Navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; //Para el funcionamiento del twoway databinding
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+
+import { routing, appRoutingProviders } from './app.routing'
 
 @NgModule({
   declarations: [
@@ -16,9 +19,12 @@ import { EmpleadoComponent } from './empleado/empleado.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

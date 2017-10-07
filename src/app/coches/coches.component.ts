e7ac3ee3ea2eda48db3c0fdc14649d719708f286
Pierre David Maldonado Diaz <pidmad92@gmail.com>
2017-10-07 15:06:17 -0500

    Peticiones Http & Ajax Json

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,15 +1,20 @@
 import{ Component } from '@angular/core';
 import{ Coche } from './coche'
+import{ PeticionesService } from '../services/peticiones.service';
 
 @Component({
     selector: 'coches',
-    templateUrl: './coches.component.html'
+    templateUrl: './coches.component.html',
+    providers: [PeticionesService]
 })
 export class CochesComponent{
     public coche: Coche;
     public coches: Array<Coche>;
+    public articulos;
 
-    constructor(){
+    constructor(
+        private _peticionesService: PeticionesService,
+    ){
 
         this.coche = new Coche ("","","");
         this.coches = [
@@ -18,6 +23,26 @@ export class CochesComponent{
             new Coche("Audi GT", "2500", "Amarillo")
         ];
 
+    }
+
+    ngOnInit(){
+        //Importante que toda funcion vaya con sus parentesis
+        //console.log(this._peticionesService.getPrueba())
+        this._peticionesService.getArticulos().subscribe(
+            result => {
+                this.articulos = result;
+
+                if(!this.articulos){ // Si viene vacio
+                    console.log("Error en el servidor");
+                }
+                //console.log(result);
+            },
+            error => {
+                var errorMenssage = <any>error;
+                console.log(errorMenssage);
+            }
+        );
+        
     }
 
     onSubmit(){
